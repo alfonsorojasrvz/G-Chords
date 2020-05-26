51e6d6a35677f74ca350b69c16749eeaffa6930d
Gunther <nanoyv6@gmail.com>
2020-05-26 11:24:07 -0700

    initial commit

---
+++ app/src/main/java/rvz/gchords/ui/main/MainActivity.java
@@ -0,0 +1,17 @@
+package rvz.gchords.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rvz.gchords.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
