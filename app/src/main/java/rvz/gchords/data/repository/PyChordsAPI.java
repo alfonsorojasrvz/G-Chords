59c2ce5b88f584a331791905924fc8c38cad7344
Gunther <nanoyv6@gmail.com>
2020-05-27 14:47:29 -0700

    Add query parameters (Note and scale name).

--- app/src/main/java/rvz/gchords/data/repository/PyChordsAPI.java
+++ app/src/main/java/rvz/gchords/data/repository/PyChordsAPI.java
@@ -2,9 +2,12 @@ package rvz.gchords.data.repository;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 import rvz.gchords.data.model.Scale;
 
 public interface PyChordsAPI {
     @GET("api/notes/")
-    Call<Scale> getScale();
+    Call<Scale> getScale(
+            @Query("note") String note,
+            @Query("scalename") String scalename);
 }
