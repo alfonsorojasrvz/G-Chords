17373ed356bd7c49ac2dfe48755a7826bcda94c8
Gunther <nanoyv6@gmail.com>
2020-05-27 11:21:09 -0700

    Create Scale data model.

---
+++ app/src/main/java/rvz/gchords/data/model/Scale.java
@@ -0,0 +1,12 @@
+package rvz.gchords.data.model;
+
+import java.util.List;
+
+public class Scale {
+
+    private List<String> response;
+
+    public List<String> getResponse() {
+        return response;
+    }
+}
--- app/src/main/java/rvz/gchords/viewmodel/MainViewModel.java
+++ app/src/main/java/rvz/gchords/viewmodel/MainViewModel.java
@@ -3,5 +3,5 @@ package rvz.gchords.viewmodel;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+// TODO: Implement the ViewModel
 }
