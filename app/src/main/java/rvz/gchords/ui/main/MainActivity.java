3ca861355b49fc04160da975a59ce3aee0981c9c
Gunther <nanoyv6@gmail.com>
2020-05-27 11:01:29 -0700

    Get ViewModel

--- app/src/main/java/rvz/gchords/ui/main/MainActivity.java
+++ app/src/main/java/rvz/gchords/ui/main/MainActivity.java
@@ -2,6 +2,8 @@ package rvz.gchords.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
@@ -11,11 +13,13 @@ import rvz.gchords.databinding.MainActivityBinding;
 
 public class MainActivity extends AppCompatActivity {
 private MainActivityBinding binding;
+private ViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         binding = DataBindingUtil.setContentView(this,R.layout.main_activity);
         binding.setLifecycleOwner(this);
+        viewModel = new ViewModelProvider(this).get(ViewModel.class);
     }
 }
