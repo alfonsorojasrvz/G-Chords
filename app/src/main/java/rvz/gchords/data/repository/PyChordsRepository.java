099889a07357fab90426caeb52105798c1b3d5de
Gunther <nanoyv6@gmail.com>
2020-05-27 13:44:55 -0700

    Create getScaleFromAPI.

--- app/src/main/java/rvz/gchords/data/repository/PyChordsRepository.java
+++ app/src/main/java/rvz/gchords/data/repository/PyChordsRepository.java
@@ -1,10 +1,31 @@
 package rvz.gchords.data.repository;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import rvz.gchords.data.model.Scale;
+
 public class PyChordsRepository {
 
     private PyChordsAPI pyChordsAPI;
 
     public PyChordsRepository() {
         pyChordsAPI = PyChordsService.getRetrofit().create(PyChordsAPI.class);
+    }
+
+    public Scale getScale(){
+
+        pyChordsAPI.getScale().enqueue(new Callback<Scale>() {
+            @Override
+            public void onResponse(Call<Scale> call, Response<Scale> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Scale> call, Throwable t) {
+
+            }
+        });
+        return null;
     }
 }
