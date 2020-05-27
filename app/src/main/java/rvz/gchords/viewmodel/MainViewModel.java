e543ed39ea9019ef924b7e19203a65157fe125c8
Gunther <nanoyv6@gmail.com>
2020-05-27 14:27:21 -0700

    Request data to Respository from Viewmodel.

--- app/src/main/java/rvz/gchords/viewmodel/MainViewModel.java
+++ app/src/main/java/rvz/gchords/viewmodel/MainViewModel.java
@@ -1,7 +1,18 @@
 package rvz.gchords.viewmodel;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import rvz.gchords.data.repository.PyChordsRepository;
+
 public class MainViewModel extends ViewModel {
-// TODO: Implement the ViewModel
+private MutableLiveData<String> scaleNotes = new MutableLiveData<>();
+
+    public void requestScaleNotes() {
+        PyChordsRepository pyChordsRepository = new PyChordsRepository();
+        String scaleNotesString = pyChordsRepository.getScale();
+        scaleNotes.setValue(scaleNotesString);
+    }
+
 }
