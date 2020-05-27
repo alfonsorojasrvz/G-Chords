2716cf71e7826537503864527efaf6b293b16f2e
Gunther <nanoyv6@gmail.com>
2020-05-27 14:27:46 -0700

    Exposing data from Viewmodel.

--- app/src/main/java/rvz/gchords/viewmodel/MainViewModel.java
+++ app/src/main/java/rvz/gchords/viewmodel/MainViewModel.java
@@ -14,5 +14,7 @@ private MutableLiveData<String> scaleNotes = new MutableLiveData<>();
         String scaleNotesString = pyChordsRepository.getScale();
         scaleNotes.setValue(scaleNotesString);
     }
-
+    public LiveData<String> getScaleNotes(){
+        return scaleNotes;
+    }
 }
