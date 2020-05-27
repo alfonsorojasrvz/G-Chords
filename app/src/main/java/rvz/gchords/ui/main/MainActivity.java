b02dad02bd6d03d250062040366059dd908663d2
Gunther <nanoyv6@gmail.com>
2020-05-27 10:57:36 -0700

    Inflate view and get binding class instance.

--- app/src/main/java/rvz/gchords/ui/main/MainActivity.java
+++ app/src/main/java/rvz/gchords/ui/main/MainActivity.java
@@ -1,17 +1,20 @@
 package rvz.gchords.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import rvz.gchords.R;
+import rvz.gchords.databinding.MainActivityBinding;
 
 
 public class MainActivity extends AppCompatActivity {
-
+private MainActivityBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        binding = DataBindingUtil.setContentView(this,R.layout.main_activity);
     }
 }
